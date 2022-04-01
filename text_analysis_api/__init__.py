d8d46abe7c212b1fb94a0684f1f3e9af6d613a4a
ab1821291660 <35366455+ab1821291660@users.noreply.github.com>
2022-04-01 16:08:10 +0800

    Add files via upload

---
+++ text_analysis_api/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from text_analysis_api.text_cluster.kmeans import KmeansClustering
+from text_analysis_api.text_cluster.dbscan import DbscanClustering
+#
+#
+#
+#
+from text_analysis_api.text_classification.fasttext import Fasttext
+from text_analysis_api.sentiment.sentiment import SentimentAnalysis
+from text_analysis_api.text_similarity.cosion import CosionSimilarity
+from text_analysis_api.text_similarity.edit import EditSimilarity
+from text_analysis_api.text_similarity.simhash import SimHashSimilarity
+#
+# from text_analysis_api.triple_extraction.triple_extraction_ltp import TripleExtraction
+#
+#caption
+#
+#mrc
+#
+from text_analysis_api.summarization.tfidf_summarization import TfidfSummarization
+from text_analysis_api.summarization.textrank_summarization import TextRankSummarization
+#
+#
+#
+#
+from text_analysis_api.spell_correct.spell_correct import SpellCorrect
+
+from text_analysis_api.keywords.tfidf import TfidfKeywords
+from text_analysis_api.keywords.textrank import TextRankKeywords
+from text_analysis_api.keyphrase.keyphrase import KeyPhraseExtraction
+from text_analysis_api.topic_keywords.topic_kwywords import TopicKeywords
+
+from text_analysis_api.synonym.word2vec import Word2VecSynonym
+from text_analysis_api.synonym.synonym_dict import SynonymDict
+
+
+
